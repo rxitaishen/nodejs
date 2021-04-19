82b3a84ec044f15f674d2cdc383fc7a22a0ddc49
wqq <1119594587@qq.com>
2021-04-18 21:11:01 -0700

    0419 biao dan

--- test.js
+++ test.js
@@ -2,19 +2,39 @@ const http = require('http')
 const fs = require("fs")
 var i=0;
 const server = http.createServer((req,res)=>{
-    if(req.url=="/"){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html')
-        var fsData = fs.readFileSync("testnew.html")
-        res.write(fsData)
-        res.end
-    }
-    else if(req.url=="/favicon.ico"){
+    if(req.url=="/favicon.ico"){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/img')
-        var fsData = fs.readFileSync("favico.ico")
-        res.write(fsData)
-        res.end
+        //var fsData = fs.readFileSync("favico.ico") 同步的
+        fs.readFile("favico.ico",(err,fsData)=>{//异步的
+            if(err){
+                console.log("Read File Error")
+                throw err
+            }
+            //console.log("1")
+            res.write(fsData)
+            res.end()
+        })
+        //console.log(2)
+        //看见1和2的出现顺序说明回调函数是先执行完主流程，再去执行这个回调函数
+    }
+    else if(req.url=="/"){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html')
+        //var fsData = fs.readFileSync("testnew.html")//同步的
+        fs.readFile("testform.html",(err,fsData)=>{
+            if(err){
+                console.log("Read File Error")
+                throw err
+            }
+            res.write(fsData)
+            res.end()
+        })
+    }
+    else if (req.url = "/input"){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/img')
+        res.end("提交成功！")
     }
     else{
         res.statusCode = 200;
