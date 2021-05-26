f89528fd974e8ed6862a7a85bfed72f07ca05a63
wqq <1119594587@qq.com>
2021-05-25 23:15:14 -0700

    xuanranle

--- test.js
+++ test.js
@@ -3,12 +3,15 @@ const fs = require("fs")
 const url = require("url")
 const querystring=require('querystring')
 const express =require('express')
+const ejs = require('ejs')
 const insertDB = require('./MongodbLib')
 const app = express()
 //var i=0;
 //app.use(express.static(__dirname+"/public"))//哪个路径在前面就默认哪个是默认的index
 //app.use(express.static(__dirname+"/private"))
 app.use(express.static(__dirname+'/public'))
+app.set("view engine","ejs")
+app.set("views","./views")
 var a =''
 var name=""
 var password=""
@@ -18,23 +21,24 @@ app.get('/input', (req, res,next) => {
     name = req.query.name
     password = req.query.password
     submit = req.query.submit1
-    //doc[name] = password
-    //res.send('Hello World!')
     a="This is a next() test."
-    //res.send('The first')
     console.log(req.query.name)
 
     if(name.length != 0 && password.length != 0)next()
-    else
-    res.send("用户名和密码都不能为空，请输入。")
+    else//但这样表单就不在了，应该用渲染，表单还在。
+    //res.send('用户名和密码不能为空')
+    res.render(__dirname+'/views/resp.ejs',{message:"用户名和密码不能为空",information:{name:'',password:''}})
 })
 app.get('/input', (req, res, next) =>{
     if(submit = '注册'){
         console.log(name)
         insertDB.inserData("mydb","mycollection",[{name:name , password:password}])
+        
     }
+    res.render(__dirname+'/views/resp.ejs',{message:"感谢您的登录",information:{name:name,password:password}})
+    //res.render(__dirname+"/views/resp.ejs", {information:{name:name,password:password}})
     //res.sendFile(__dirname +'/testnew.html')
-    res.send(a)
+    //res.send(a)
 })
 //app.get('/input', (req, res,next) => {
  //   res.send(a)
